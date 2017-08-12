f122c0308cbdbcd7c2a466781fa3191d42e3874d
Samuel Holland <samuel@sholland.org>
2017-08-12 04:07:39 -0500

    sun50i: Add platform PSCI functions required for SMP
    The reset vector entry point is preserved across CPU resets, so it only
    needs to be set once at boot.
    
    With this commit, Linux is able to enable and use all four CPUs.
    
    Signed-off-by: Samuel Holland <samuel@sholland.org>

--- plat/allwinner/sun50i/sun50i_pm.c
+++ plat/allwinner/sun50i/sun50i_pm.c
@@ -8,11 +8,39 @@
 #include <assert.h>
 #include <debug.h>
 #include <delay_timer.h>
+#include <gicv2.h>
 #include <mmio.h>
 #include <platform.h>
 #include <platform_def.h>
 #include <psci.h>
+#include <sun50i_cpucfg.h>
 #include <sun50i_timer.h>
+
+#include "sun50i_private.h"
+
+#define mpidr_is_valid(mpidr) (plat_core_pos_by_mpidr(mpidr) >= 0)
+
+uintptr_t warm_entrypoint;
+
+static int sun50i_pwr_domain_on(u_register_t mpidr)
+{
+	if (!mpidr_is_valid(mpidr))
+		return PSCI_E_INTERN_FAIL;
+
+	mmio_write_32(SUN50I_CPUCFG_RVBAR_LO_REG(MPIDR_AFFLVL0_VAL(mpidr)),
+		      warm_entrypoint & 0xffffffff);
+	mmio_write_32(SUN50I_CPUCFG_RVBAR_HI_REG(MPIDR_AFFLVL0_VAL(mpidr)),
+		      warm_entrypoint >> 32);
+	sun50i_cpu_on(MPIDR_AFFLVL1_VAL(mpidr), MPIDR_AFFLVL0_VAL(mpidr));
+
+	return PSCI_E_SUCCESS;
+}
+
+static void sun50i_pwr_domain_on_finish(const psci_power_state_t *target_state)
+{
+	gicv2_pcpu_distif_init();
+	gicv2_cpuif_enable();
+}
 
 static void __dead2 sun50i_system_off(void)
 {
@@ -35,9 +63,22 @@ static void __dead2 sun50i_system_reset(void)
 	panic();
 }
 
+static int sun50i_validate_ns_entrypoint(uintptr_t ns_entrypoint)
+{
+	/* The non-secure entry point must be in DRAM */
+	if (ns_entrypoint >= SUN50I_DRAM_BASE &&
+	    ns_entrypoint < SUN50I_DRAM_BASE + SUN50I_DRAM_SIZE)
+		return PSCI_E_SUCCESS;
+
+	return PSCI_E_INVALID_ADDRESS;
+}
+
 static plat_psci_ops_t sun50i_psci_ops = {
+	.pwr_domain_on			= sun50i_pwr_domain_on,
+	.pwr_domain_on_finish		= sun50i_pwr_domain_on_finish,
 	.system_off			= sun50i_system_off,
 	.system_reset			= sun50i_system_reset,
+	.validate_ns_entrypoint		= sun50i_validate_ns_entrypoint,
 };
 
 int plat_setup_psci_ops(uintptr_t sec_entrypoint,
@@ -45,6 +86,7 @@ int plat_setup_psci_ops(uintptr_t sec_entrypoint,
 {
 	assert(psci_ops);
 
+	warm_entrypoint = sec_entrypoint;
 	*psci_ops = &sun50i_psci_ops;
 
 	return 0;
